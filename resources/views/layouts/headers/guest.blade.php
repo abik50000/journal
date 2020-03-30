21f8e7dfae9fe4e4647702b3e13afb5f78630591
abik50000 <abik50000@gmail.com>
2020-03-30 17:19:27 +0600

    c

--- resources/views/layouts/headers/guest.blade.php
+++ resources/views/layouts/headers/guest.blade.php
@@ -3,7 +3,7 @@
         <div class="header-body text-center mb-7">
             <div class="row justify-content-center">
                 <div class="col-lg-5 col-md-6">
-                    <h1 class="text-white">{{ __('Добро пожаловать в Дневник школьника') }}</h1>
+                    <h1 class="text-white
                 </div>
             </div>
         </div>
