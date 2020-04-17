f83365cf8d878fbb820db3f6155438444d3bfa0f
abik50000 <abik50000@gmail.com>
2020-04-18 01:18:01 +0600

    'a'

--- resources/views/layouts/navbars/menu/student.blade.php
+++ resources/views/layouts/navbars/menu/student.blade.php
@@ -1,21 +1,24 @@
- <li class="nav-item">
+<li class="nav-item">
     <a class="nav-link" href="{{ route('home') }}">
-        <i class="ni ni-tv-2 text-primary"></i> {{ __('Панель') }}
+        <i class="ni ni-tv-2 text-primary"></i>
+        <span>{{ __('Панель') }}</span> 
     </a>
 </li>
-
 <li class="nav-item">
     <a class="nav-link" href="#">
-        <i class="ni ni-planet text-primary"></i> {{ __('Расписание') }}
+        <i class="ni ni-planet text-primary"></i> 
+        <span>{{ __('Расписание') }}</span> 
     </a>
 </li>
 <li class="nav-item">
     <a class="nav-link" href="{{ route('student_diary') }}">
-        <i class="ni ni-pin-3 text-primary"></i> {{ __('Дневник') }}
+        <i class="ni ni-pin-3 text-primary"></i> 
+        <span>{{ __('Дневник') }}</span> 
     </a>
 </li>
 <li class="nav-item">
     <a class="nav-link" href="{{ route('student_notifications') }}">
-        <i class="ni ni-bell-55 text-primary"></i> {{ __('Уведомления') }}
+        <i class="ni ni-bell-55 text-primary"></i> 
+        <span>{{ __('Уведомления') }}</span> 
     </a>
 </li>
