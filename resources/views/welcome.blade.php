cbf3e8a388eba41a3a675f5ccb998a6d22db7b62
abik50000 <abik50000@gmail.com>
2020-04-19 23:04:06 +0600

    'a'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="header py-7 py-lg-8">
+    <div class="header py-4 py-lg-4">
         <div class="container">
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
                         <img src="/img/welcome.svg" class="welcome-img" alt="">
-                        <h1 class="text-white">{{ __('Добро пожаловать в Дневник школьника!') }}</h1>
+                        <h1>{{ __('Добро пожаловать в Дневник школьника!') }}</h1>
                         <a href="/login" сlass="btn btn-success">Войти</a>
                     </div>
                 </div>
