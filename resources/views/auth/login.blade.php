fd32226610980132be395e118220276caae77b29
abik50000 <abik50000@gmail.com>
2020-04-19 23:30:32 +0600

    'a'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
  
-                    <div class="card-body px-lg-5 py-lg-5">
+                    <div class="card-body px-lg-9 py-lg-9">
 
                         <form role="form" method="POST" action="{{ route('login') }}">
                             @csrf
@@ -52,20 +52,7 @@
                         </form>
                     </div>
                 </div>
-                <div class="row mt-3">
-                    <div class="col-6">
-                        @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}" class="text-light">
-                                <small>{{ __('Забыли пароль?') }}</small>
-                            </a>
-                        @endif
-                    </div>
-                    <!-- <div class="col-6 text-right">
-                        <a href="{{ route('register') }}" class="text-light">
-                            <small>{{ __('Создать аккаунт') }}</small>
-                        </a>
-                    </div> -->
-                </div>
+        
             </div>
         </div>
     </div>
