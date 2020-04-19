ae597f82d2f6108cae97c745b149c3256845b526
abik50000 <abik50000@gmail.com>
2020-04-19 22:59:54 +0600

    'a'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
+                        <img src="/img/welcome.svg" class="welcome-img" alt="">
                         <h1 class="text-white">{{ __('Добро пожаловать в Дневник школьника!') }}</h1>
                         <a href="/login" сlass="btn btn-success">Войти</a>
                     </div>
