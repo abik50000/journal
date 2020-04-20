c0eb9e63bc4beed5426d5794522da62dfe9978bf
abik50000 <abik50000@gmail.com>
2020-04-21 01:31:30 +0600

    'a'

---
+++ resources/views/student/achievements.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+ 
+@section('content')
+
+<div class="container-fluid pt-9">
+        <div class="row">
+            <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0"></div>
+            <div class="col-xl-8 order-xl-1"></div>
+        </div>
+        
+  
+</div>
+
+
+
+
+@endsection
+@section('scripts')
+
+@endsection
