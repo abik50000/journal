cc0b7ace41613636128d8844e8371c3623f636be
abik50000 <abik50000@gmail.com>
2020-04-21 01:30:32 +0600

    'a'

--- resources/views/student/diary.blade.php
+++ resources/views/student/diary.blade.php
@@ -1,74 +1,9 @@
 @extends('layouts.app')
 @section('content')
-@include('layouts.headers.cards')
-<style>
-.mark-btn {
-    padding: 0.3rem 0.75rem;
-    border-radius: 0;
-    border: none !important;
-}
-.notification {
-    color: #eee;
-}
-.notification span {
-    width: 8px;
-    height: 8px;
-    display: inline-block;
-    background: #8bc34a;
-    animation: greenlight 2s ease infinite;
-    border-radius: 50%;
-    position: relative;
-    top: -2px;
-}
-@keyframes greenlight {
-    from {
-        
-    }
-    50% {
-        box-shadow: 0 0 5px #8bc34a;
-    }
-    to {
 
-    }
-}
-.card .table td, .card .table th {
-    padding-right: 0;
-    padding-left: 0;
-}
-.table th, .table td {
-    padding: 2px 3px;
-}
-.mark-cell {
-    text-align: center;
-    background: #fcf8e3;
-}
-.day-cell {
-    text-align: center;
-}
-</style>
-<style>
-.mark-btn[data-id="1"] {background: #4caf50;}
-.mark-btn[data-id="2"] {background: #8bc34a;}
-.mark-btn[data-id="3"] {background: #ff7600;}
-.mark-btn[data-id="4"] {background: #ff3131;}
-.mark-btn[data-id="5"] {background: #111;}
-</style>
-
-<style>
-.table {
-    background: #fff;
-}
-td.type-1,
-th.type-1 {
-    background: #7db1ff;
-}
-.card .table td:first-child, .card .table th:first-child {
-    padding: 3px 5px;
-}
-</style>
 
 <div id="app">
-    <div class="container mt--7">
+    <div class="container">
         <div class="row">
             <div class="col-xl-12 mb-5 mb-xl-0">
                 <div class="card bg-gradient-default shadow">
